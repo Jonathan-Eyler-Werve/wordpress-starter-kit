78fe694b0ecf039e6e33c46b69489b31a310128d
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-07-30 13:44:40 -0500

    nerved homepage by swapping with blog template

--- wordpress/wp-content/themes/happening_now/home.php
+++ wordpress/wp-content/themes/happening_now/home.php
@@ -1,93 +1,50 @@
 <?php get_header(); ?>
-
-<?php if ( '1' == of_get_option('display_slideshow')) { ?>
-	
-	<!-- BEGIN .slideshow -->
-    <div class="slideshow featured-slideshow">
-    
-    	<!-- BEGIN .row -->
-    	<div class="row">
-    		
-    		<!-- BEGIN .sixteen columns -->
-    	    <div class="sixteen columns">
-    	    	
-    	    	<!-- BEGIN .slideshow -->
-    	    	<div class="slideshow">
-    
-			        <!-- BEGIN .flexslider -->
-			        <div class="flexslider loading" data-speed="<?php echo of_get_option('transition_interval'); ?>">
-			        
-			            <!-- BEGIN ul.slides -->
-			            <ul class="slides">
-			            
-			                <?php get_template_part( 'loop', 'slider' ); ?>
-			
-			            <!-- END ul.slides -->
-			            </ul>
-			            
-			        <!-- END .flexslider -->
-			        </div>
-		        
-		        <!-- END .slideshow -->
-		        </div>
-	        
-	        <!-- END .sixteen columns -->
-	        </div>
-            
-        <!-- END .row -->
-        </div>
-    
-    <!-- END .slideshow -->
-    </div>
-	    
+ 
+<?php if ( '' != get_the_post_thumbnail() ) { $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'banner'); } ?>
+<?php if ( '' != get_the_post_thumbnail() ) { ?>
+	<div class="banner" style="background-image: url(<?php echo $thumb[0]; ?>);"><span class="banner-img"><?php the_post_thumbnail( 'response-banner' ); ?></span></div>
 <?php } ?>
 
-<?php if ( '1' == of_get_option('display_home')) { ?>
-
 <!-- BEGIN #content -->
-<div id="content">
+<div <?php post_class(); ?> id="content">
 
 	<!-- BEGIN .row -->
 	<div class="row">
-		    
-	    <div id="homepage">
+	
+		<?php if ( is_active_sidebar( 'sidebar-blog' ) ) : ?>
 	    
-	    	<div class="sixteen columns">
-	    	
-				<?php $wp_query = new WP_Query('page_id='.of_get_option('select_page')); while($wp_query->have_posts()) : $wp_query->the_post(); ?>
-				<?php global $more; $more = 0; ?>
-
-				<div class="article">
+		    <!-- BEGIN .eleven columns -->
+		    <div id="blog" class="eleven columns">
 				
-					<?php the_content(__("Read More", 'organicthemes')); ?>
-					
-					<?php wp_link_pages(array(
-						'before' => '<p class="page-links"><span class="link-label">' . __('Pages:', 'organicthemes') . '</span>',
-						'after' => '</p>',
-						'link_before' => '<span>',
-						'link_after' => '</span>',
-						'next_or_number' => 'next_and_number',
-						'nextpagelink' => __('Next', 'organicthemes'),
-						'previouspagelink' => __('Previous', 'organicthemes'),
-						'pagelink' => '%',
-						'echo' => 1 )
-					); ?>
-
-				</div>
+				<?php get_template_part( 'loop', 'blog' ); ?>
 				
-				<?php endwhile; ?>
-			
+			<!-- END .eleven columns -->
 			</div>
+			
+			<!-- BEGIN .five columns -->
+			<div class="five columns">
+			
+			    <?php get_sidebar(); ?> 
+			
+			<!-- END .five columns -->
+			</div>
+			
+		<?php else : ?>
 		
-		<!-- END #homepage -->
-		</div>
-		
+			<!-- BEGIN .sixteen columns -->
+			<div id="blog" class="sixteen columns">
+				
+				<?php get_template_part( 'loop', 'blog' ); ?>
+				
+			<!-- END .sixteen columns -->
+			</div>
+			
+		<?php endif; ?>
+	
 	<!-- END .row -->
 	</div>
 
 <!-- END #content -->
 </div>
-
-<?php } ?>
 
 <?php get_footer(); ?>
